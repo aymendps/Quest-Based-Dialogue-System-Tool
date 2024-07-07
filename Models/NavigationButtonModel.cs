5510ed167f5c58d49e0f4b50e786879e6cb601cc
Aymen Hammami <hammami.aym@outlook.com>
2024-07-07 20:30:25 +0100

    implemented navigation button model, view and viewmodel

--- Models/NavigationButtonModel.cs
+++ Models/NavigationButtonModel.cs
@@ -2,7 +2,30 @@
 {
     class NavigationButtonModel
     {
-        public string Name { get; set; } = "Missing Name";
-        public string Icon { get; set; } = "Missing Icon Path";
+        /// <summary>
+        /// The name of the navigation button.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The path to the icon/image of the navigation button.
+        /// </summary>
+        public string IconPath { get; set; }
+
+        /// <summary>
+        /// Whether the navigation button is currently selected.
+        /// </summary>
+        public bool IsSelected { get; set; }
+
+        public NavigationButtonModel() : this("Missing Name", "Missing Icon Path", false)
+        {
+        }
+
+        public NavigationButtonModel(string name, string iconPath, bool isSelected)
+        {
+            Name = name;
+            IconPath = iconPath;
+            IsSelected = isSelected;
+        }
     }
 }
--- ViewModels/NavigationButtonViewModel.cs
+++ ViewModels/NavigationButtonViewModel.cs
@@ -1,12 +1,47 @@
-﻿using System;
+﻿using QuestBasedDialogueSystemTool.Commands;
+using QuestBasedDialogueSystemTool.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace QuestBasedDialogueSystemTool.ViewModels
 {
     class NavigationButtonViewModel : BaseViewModel
     {
+        private NavigationButtonModel _model;
+
+        public string Name
+        {
+            get => _model.Name;
+        }
+
+        public string IconPath
+        {
+            get => _model.IconPath;
+        }
+
+        public bool IsSelected
+        {
+            get => _model.IsSelected;
+            set
+            {
+                _model.IsSelected = value;
+                OnPropertyChanged(nameof(IsSelected));
+            }
+        }
+
+        /// <summary>
+        /// Command used to navigate to the corresponding view
+        /// </summary>
+        public ICommand NavigateCommand { get; }
+
+        public NavigationButtonViewModel(string name, string iconPath, bool isSelected, Action<object> navigateAction)
+        {
+            _model = new NavigationButtonModel(name, iconPath, isSelected);
+            NavigateCommand = new RelayCommand<object>(navigateAction);
+        }
     }
 }
