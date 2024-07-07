b677a80dd5b7d1080f8bedc4d9b90d8dfde741f7
Aymen Hammami <hammami.aym@outlook.com>
2024-07-07 20:19:40 +0100

    Added aspect ratio converter that allows setting height or width of an element based on the other using a multiplier (default 1 for 1:1 aspect ratio).

---
+++ Converters/AspectRatioConverter.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace QuestBasedDialogueSystemTool.Converters
+{
+    class AspectRatioConvertor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double multiplier = 1.0;
+
+            if (parameter != null && double.TryParse(parameter.ToString(), out double result))
+            {
+                multiplier = result;
+            }
+
+            return (double)value * multiplier;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            double multiplier = 1.0;
+
+            if (parameter != null && double.TryParse(parameter.ToString(), out double result))
+            {
+                multiplier = result;
+            }
+
+            return (double)value / multiplier;
+        }
+    }
+}
