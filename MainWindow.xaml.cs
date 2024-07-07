5da507c743b41ad9ac1f3c178d4289b1f84c2e58
Aymen Hammami <hammami.aym@outlook.com>
2024-07-07 20:30:50 +0100

    added main view model and its logic to spawn the navigation bar and its buttons

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using QuestBasedDialogueSystemTool.ViewModels;
 
 namespace QuestBasedDialogueSystemTool
 {
@@ -22,6 +23,7 @@ namespace QuestBasedDialogueSystemTool
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new MainViewModel();
         }
     }
 }
---
+++ ViewModels/MainViewModel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuestBasedDialogueSystemTool.ViewModels
+{
+    class MainViewModel : BaseViewModel
+    {
+        private ObservableCollection<NavigationButtonViewModel> _navigationButtons;
+        public ObservableCollection<NavigationButtonViewModel> NavigationButtons
+        {
+            get => _navigationButtons;
+        }
+
+        private int _selectedNavigationButtonIndex;
+        public int SelectedNavigationButtonIndex
+        {
+            get => _selectedNavigationButtonIndex;
+            set
+            {
+                _selectedNavigationButtonIndex = value;
+                UpdateNavigationButtons();
+                OnPropertyChanged(nameof(SelectedNavigationButtonIndex));
+            }
+        }
+
+        public MainViewModel()
+        {
+            Trace.WriteLine("MainViewModel created");
+            _navigationButtons = new ObservableCollection<NavigationButtonViewModel>
+            {
+                new("NPCs", "/Assets/Icons/npc.png", true, (object parameter) =>
+                {
+                    Trace.WriteLine("Navigate to NPCs");
+                    SelectedNavigationButtonIndex = 0;  
+                }),
+                new("Quests", "/Assets/Icons/quest.png", false, (object parameter) =>
+                {
+                    Trace.WriteLine("Navigate to Quests");
+                    SelectedNavigationButtonIndex = 1;
+                }),
+                new("Dialogues", "/Assets/Icons/dialogue.png", false, (object parameter) =>
+                {
+                    Trace.WriteLine("Navigate to Dialogues");
+                    SelectedNavigationButtonIndex = 2;
+                }),
+            };
+        }
+
+        private void UpdateNavigationButtons()
+        {
+            for (int i = 0; i < _navigationButtons.Count; i++)
+            {
+                _navigationButtons[i].IsSelected = i == _selectedNavigationButtonIndex;
+            }
+        }
+    }
+}
