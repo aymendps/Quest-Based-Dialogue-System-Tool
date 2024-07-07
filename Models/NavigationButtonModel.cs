2c8e27124deb0c08eb8fc0901c60e4e440612813
Aymen Hammami <hammami.aym@outlook.com>
2024-07-07 11:52:02 +0100

    first commit

---
+++ Models/NavigationButtonModel.cs
@@ -0,0 +1,8 @@
+﻿namespace QuestBasedDialogueSystemTool.Models
+{
+    class NavigationButtonModel
+    {
+        public string Name { get; set; } = "Missing Name";
+        public string Icon { get; set; } = "Missing Icon Path";
+    }
+}
